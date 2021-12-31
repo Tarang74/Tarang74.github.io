3fac7360b1f3e730d870ce78fc3935a1ade8c941
Tarang74 <tjanawalkar74@gmail.com>
2021-12-31 21:51:10 +1000

    Removed compiled files.

--- src/ts/unit_select.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const network_api_1 = require("./network_api");
-(0, network_api_1.dataToNetwork)([5]);
-//# sourceMappingURL=unit_select.js.map
