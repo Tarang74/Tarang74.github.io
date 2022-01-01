9d8779b982d8d2b9479fae3be923433c8ba6327e
Tarang74 <tjanawalkar74@gmail.com>
2022-01-02 00:00:51 +1000

    Modified how units are displayed in selection. Finished styles for unit selection.

--- src/ts/unit_select.ts
+++ src/ts/unit_select.ts
@@ -15,58 +15,76 @@ type Unit = [string, Array<Array<string> | string>];
 function updateOptions() {
     var input: string = select.value.toUpperCase();
 
-    if (!appendUnits) {
-        for (let i = 0; i < units.length; i++) {
-            let a1: HTMLAnchorElement = document.createElement('a');
-            a1.style.cssText = 'width:100%;height:50%;padding:0.5rem;';
-            a1.textContent = units[i];
-
-            let a2: HTMLAnchorElement = document.createElement('a');
-            a2.style.cssText = 'width:100%;height:50%;padding:0.5rem;';
-            a2.textContent = 'Unit Name';
-
-            let div: HTMLDivElement = document.createElement('div');
-            div.style.cssText = 'width:100%;display:flex;flex-direction:column;';
-            div.appendChild(a1);
-            div.appendChild(a2);
-
-            let li: HTMLLIElement = document.createElement('li');
-            li.id = i.toString();
-            li.style.cssText = 'width:100%;height:6rem;';
-            li.appendChild(div);
-
-            ul.appendChild(li);
-        }
-        appendUnits = true;
-    }
-    
-    var li = [...ul.getElementsByTagName("li")];
-    li.forEach(element => {
-        element.addEventListener('click', () => {addUnitsToSelection(parseInt(element.id)); closeOptions()});
-    });
-
     if (input != '') {
+        let hidden: number = 0;
+        let visible: Array<number> = [];
+
         openOptions();
-        for (let i = 0; i < li.length; i++) {
+
+        for (let i = 0; i < units.length; i++) {
             if (units[i].indexOf(input) > -1) {
-                li[i].style.display = "";
+                visible.push(i);
             } else {
-                li[i].style.display = "none";
+                hidden++;
             }
         }
+
+        if (hidden == units.length) {
+            // No units found
+            closeOptions();
+        } else {
+            for (let i = 0; i < visible.length; i++) {
+                let a1: HTMLAnchorElement = document.createElement('a');
+                a1.textContent = units[visible[i]];
+
+                let a2: HTMLAnchorElement = document.createElement('a');
+                a2.textContent = 'Unit Name';
+
+                let div: HTMLDivElement = document.createElement('div');
+                div.appendChild(a1);
+                div.appendChild(a2);
+
+                let li: HTMLLIElement = document.createElement('li');
+                li.id = visible[i].toString();
+                li.appendChild(div);
+
+                ul.appendChild(li);
+            }
+
+            var li = [...ul.getElementsByTagName("li")];
+            li.forEach(element => {
+                element.addEventListener('click', () => { addUnitsToSelection(parseInt(element.id)); closeOptions() });
+            });
+        }
+    } else {
+        closeOptions();
     }
+}
+
+function checkForScroll() {
+    let selectContainer = document.getElementById("select-units-container");
+    let dim = selectContainer.getBoundingClientRect();
+    let top = dim.y + dim.height;
+
+    ul.parentElement.style.top = `${top.toString()}px`;
 }
 
 function openOptions() {
+    clearOptions();
     ul.style.display = '';
 }
 
+function clearOptions() {
+    while (ul.firstChild) ul.removeChild(ul.firstChild);
+}
+
 function closeOptions() {
+    clearOptions(); 
     ul.style.display = 'none';
 }
 
 function addUnitsToSelection(l: number | Array<number>) {
-    if (typeof(l) == 'object') {
+    if (typeof (l) == 'object') {
         for (let i = 0; i < l.length; i++) {
             selectedUnits.push(units[l[i]]);
         }
@@ -83,7 +101,7 @@ function selectUnits(): NetworkNode {
     // // Find units in URL
     // var url = new URL(window.location.href);
     // selectedUnits = url.searchParams.get("units").split(',').map(Number);
-    
+
     // Use indices to find nodes (and children)
     var recursiveUnits: Unit[] = [];
     for (let i = 0; i < selectedUnits.length; i++) {
@@ -92,7 +110,7 @@ function selectUnits(): NetworkNode {
 
     var nodes = formatNode(findRecursive(recursiveUnits));
     console.log(nodes);
-    
+
     return { nodes: [{ name: 'A' }], links: [{ source: 'A', target: 'B' }] };
 }
 
@@ -142,9 +160,6 @@ data.Units.forEach((value, index, array) => { units.push(value.unit_code); prere
 
 var selectedUnits: Array<string> = [];
 
-// Append units to search input
-var appendUnits: boolean = false;
-
 // Selection
 /* Event listener */
 const select = <HTMLInputElement>document.getElementById("select-units");
@@ -153,5 +168,8 @@ export var output: NetworkNode;
 /* Options list */
 var ul: HTMLElement = document.getElementById("select-options");
 
-select.addEventListener("input", updateOptions);
-select.addEventListener("blur", updateOptions);
+select.addEventListener("input", () => {updateOptions(); checkForScroll()});
+select.addEventListener("blur", closeOptions);
+
+document.addEventListener("click", closeOptions);
+closeOptions();
