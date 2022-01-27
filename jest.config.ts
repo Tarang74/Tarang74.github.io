c1f5ca0a54c9a2c5bc7337e68f41ef20d88bb53a
Tarang74 <tjanawalkar74@gmail.com>
2022-01-28 02:18:54 +1000

    Added path aliases to config files and moved tests folder to src

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,15 @@
 import type { Config } from '@jest/types';
+import { pathsToModuleNameMapper } from 'ts-jest';
+import { compilerOptions } from './tsconfig.paths.json';
 
 // Sync object
 const config: Config.InitialOptions = {
     preset: 'ts-jest',
-    testEnvironment: 'node'
-}
+    testEnvironment: 'node',
+
+    moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+        prefix: '<rootDir>/'
+    })
+};
 
 export default config;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-// import sassDts from 'vite-plugin-sass-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
